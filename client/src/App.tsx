4e773495f66b40fda5a3d3dbf86c5b2865636d2f
ghannoum74 <abdelrahmangh74@gmail.com>
2024-08-05 15:08:34 +0300

    maping through the inputs data and passed data to eachForm component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import EachForm from "./components/EachForm";
+import { inputsData } from "./utilities/inputsData";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {inputsData.map((item) => (
+        <EachForm key={item.id} item={item} />
+      ))}
+    </>
+  );
 }
 
 export default App;
