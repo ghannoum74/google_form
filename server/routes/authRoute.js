6073267e8b59cbd1c2aee675ebde788a0e9212ec
ghannoum74 <abdelrahmangh74@gmail.com>
2024-08-08 23:57:39 +0300

    Add checking email or password exist routes

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 const createNewUser = require("../controllers/signup");
 const login = require("../controllers/login");
+const { checkEmailExists } = require("../controllers/checkEmailExists");
+const { checkPhoneExists } = require("../controllers/checkPhoneExists");
+
 router.post("/signup", createNewUser);
 router.post("/login", login);
 
+router.post("/check-email", checkEmailExists);
+router.post("/check-phone", checkPhoneExists);
 module.exports = router;
